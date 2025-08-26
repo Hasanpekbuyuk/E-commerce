71eef5c2fb5de19d29d3b2a299f23273501f856c
Hasan <hasan-pekbuyuk@outlook.com>
2025-08-26 16:56:38 +0300

    T17 güncelleme

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/ShopPage.jsx
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
